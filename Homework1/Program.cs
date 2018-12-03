dc28ed3431adbdc0ea8848cb2e49e2d3e5d06ab9
Andrei <cimpoes.andrei.alexandru@gmail.com>
2018-12-03 23:18:33 +0200

    Guess number game

---
+++ Homework1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rng = new Random();
+            int r = rng.Next(0, 100);
+            while (true)
+            {
+                Console.WriteLine("Type the number to guess:");
+                string st = Console.ReadLine();
+                int number = int.Parse(st);
+                if (number < r)
+                    Console.WriteLine("The number is too small");
+                if (number > r)
+                    Console.WriteLine("The number is too big");
+                if (number == r)
+                {
+                    Console.WriteLine("The number is {0}", number);
+                    break;
+                }
+            }
+        }
+    }
+}
